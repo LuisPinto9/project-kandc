f01c0a4706498219254bf52311685e8e739791d2
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2023-06-22 11:44:51 -0500

    Establecimiento de algunos coomponentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      {/* <Card></Card> */}
+      <Footer />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/styles.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar-red">
+        <h1>Navbar</h1>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
