7ee92819fa3ccf0d01cda000e907c7dbd937e337
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2023-06-25 17:13:54 -0500

    Cambios del Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,19 +6,19 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div>
-      <div className="footer-principal d-flex">
-        <footer className="align-items-center">
-        <Link className="navbar-brand align-items-center " to="/">
+      <div className="footer-principal">
+        <footer className="d-flex">
+          <Link className="navbar-brand align-items-center " to="/">
             <div className="d-flex d-inline-block align-items-center">
-              <h1 className="titulo-footer px-2 pt-3">Edificio K&C</h1>
-              <img
-                src={edificio}
-                alt="Logo"
-                width="45"
-                height="45"
-              />
+              <h4 className="titulo-footer px-2 pt-3">Edificio K&C</h4>
+              <img src={edificio} alt="Logo" width="45" height="45" />
             </div>
           </Link>
+          <div className=" d-flex px-5">
+          <h4 className="titulo-footer px-5 pt-3">Carrera</h4>
+          <h4 className="titulo-footer px-5 pt-3">334233423</h4>
+          <h4 className="titulo-footer px-5 pt-3">facebook</h4>
+          </div>
         </footer>
       </div>
     </div>
