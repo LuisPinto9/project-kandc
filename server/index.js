976c872e41736344c632660e054a5b2513fdeed2
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2023-07-09 17:26:38 -0500

    arreglo de los imput

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app= express();
+const app = express();
 const mysql = require("mysql");
 
 const cors = require("cors");
@@ -7,44 +7,58 @@ app.use(cors());
 app.use(express.json());
 
 
-const db= mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"proyectoSoftware"
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "proyectoSoftware"
 });
 
-app.post("/create",(req,res)=>{
+app.post("/create", (req, res) => {
     const IDUsuario = req.body.IDUsuario;
     const Nombre = req.body.Nombre;
-    const PhoneNumber = req.body. PhoneNumber;
-    db.query('insert into Arrendatario1(ID,nombre,telefono) values(?,?,?)',[IDUsuario,Nombre,PhoneNumber],
-    (err,result)=>{
-        if(err){
-            console.log(err);
-        }else{
-            console.log("tal cosa");
-            res.send("arrendatario registradosss");
-        }
-    }
-);
-
-/* 
-
-    db.query('insert into Arrendatario1(ID,mombre,telefono) values(?,?,?)',['1','diana','3123213243']
-    db.query('insert into Arrendatario1(ID,mombre,telefono) values(?,?,?)',[IDUsuario,Nombre,PhoneNumber],
-        (err,result)=>{
-            if(err){
+    const PhoneNumber = req.body.PhoneNumber;
+    db.query('insert into Arrendatario1(ID,nombre,telefono) values(?,?,?)', [IDUsuario, Nombre, PhoneNumber],
+        (err,result) => {
+            if (err) {
                 console.log(err);
-            }else{
+            } else {
                 console.log("tal cosa");
                 res.send("arrendatario registradosss");
             }
         }
     );
- */
+
 });
 
-app.listen(3001,()=>{
+
+
+
+
+app.get("/Arrendatarios", (req, res) => {
+    db.query('select * from Arrendatario1',
+        (err,result1) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("mostrar en la tabla");
+                res.send(result1);
+            }
+        }
+    );
+
+    /* 
+        db.query('insert into Arrendatario1(ID,mombre,telefono) values(?,?,?)',['1','diana','3123213243']
+        
+     */
+});
+
+
+
+
+
+
+
+app.listen(3001, () => {
     console.log("Corriendo en puerto3001")
 })
--- src/pages/inventario.js
+++ src/pages/inventario.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import BarraLateral from "../components/BarraLateral";
 import "../css/styles.css";
 import "../css/inventario.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import logo from "../logo.svg";
 import "../App.css";
@@ -10,30 +11,53 @@ import Axios from "axios";
 
 function Inventario() {
 
+  /* datos de los arrendatarios */
+  const [IDUsuario, setIDUsuario] = useState("");
+  const [Nombre, setNombre] = useState("");
+  const [Apellido, setApellido] = useState("");
+  const [TypeUser, setTypeUser] = useState("");
+  const [Identification, setIdentification] = useState("");
+  const [Contraseña, setContraseña] = useState("");
+  const [PhoneNumber, setphoneNumber] = useState("");
+  const [Gmail, setGmail] = useState("");
+  const [LeaseDate, setLeaseDate] = useState("");
+  const [State1, setState1] = useState("");
 
-  const [IDUsuario,setIDUsuario]=useState("");
-  const [Nombre,setNombre]=useState("");
-  const [Apellido,setApellido]=useState("");
-  const [TypeUser,setTypeUser]=useState("");
-  const [Identification,setIdentification]=useState("");
-  const [Contraseña,setContraseña]=useState("");
-  const [PhoneNumber,setphoneNumber]=useState("");
-  const [Gmail,setGmail]=useState("");
-  const [LeaseDate,setLeaseDate]=useState("");
-  const [State1,setState1]=useState("");
-  
+  const [ArrendatarioList, setArrendatarioList] = useState([]);
 
-  const add=()=>{
+  /* agregar arrendatarios */
+  const add = () => {
 
-    Axios.post("http://localhost:3001/create",{
-    IDUsuario:IDUsuario,  
-    Nombre:Nombre,
-      PhoneNumber:PhoneNumber
-    }).then(()=>{
-
+    Axios.post("http://localhost:3001/create", {
+      IDUsuario: IDUsuario,
+      Nombre: Nombre,
+      PhoneNumber: PhoneNumber
+    }).then(() => {
       alert("se registro bro");
-    })
+    });
   }
+
+  /* mostrar arrendatarios */
+  const getArrendatario = () => {
+
+    Axios.get("http://localhost:3001/Arrendatarios").then((response) => {
+      setArrendatarioList(response.data);
+      alert("mostrar datos");
+      {
+
+        ArrendatarioList.map((val, key) => {
+          return
+          <label>
+            {val.Nombre}
+          </label>
+
+        })
+
+      }
+
+    });
+  }
+
 
 
   return (
@@ -42,74 +66,129 @@ function Inventario() {
         <div className="col-2 div-barra">
           <BarraLateral />
         </div>
+        <div className="container">
+
+          <div className="inventario1">
+
+            {/*  map-pasar por todos los valores */}
+            <div className="lista">
+              <button onClick={getArrendatario}> Listar </button>
+              <label>holaa</label>
+            </div>
+          </div>
+
+
+          <div className="card text-center">
+            <div className="card-header">
+              Gestion de Arrendatario
+            </div>
+            <div className="card-body">
+
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">ID del Usuario </span>
+                <input type="number"
+                 onChange={(event) => {
+                  setIDUsuario(event.target.value);
+                }}
+                className="form-control"  aria-label="Username" aria-describedby="basic-addon1"/>
+              </div>
+
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">Nombre </span>
+                <input type="text"
+                 onChange={(event) => {
+                  setNombre(event.target.value);
+                }}
+                className="form-control" aria-label="Username" aria-describedby="basic-addon1"/>
+              </div>
+
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">Apellido </span>
+                <input type="text"
+                 onChange={(event) => {
+                  setApellido(event.target.value);
+                }}
+                className="form-control"  aria-label="Username" aria-describedby="basic-addon1"/>
+              </div>
+
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">Tipo de Usuario </span>
+                <input type="text"
+                 onChange={(event) => {
+                  setTypeUser(event.target.value);
+                }}
+                className="form-control"  aria-label="Username" aria-describedby="basic-addon1"/>
+              </div>
+
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">Contraseña</span>
+                <input type="text"
+                 onChange={(event) => {
+                  setContraseña(event.target.value);
+                }}
+                className="form-control"  aria-label="Username" aria-describedby="basic-addon1"/>
+              </div>
+
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">Identificacion</span>
+                <input type="text"
+                 onChange={(event) => {
+                  setIdentification(event.target.value);
+                }}
+                className="form-control"  aria-label="Username" aria-describedby="basic-addon1"/>
+              </div>
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">Correo Electronico</span>
+                <input type="text"
+                 onChange={(event) => {
+                  setGmail(event.target.value);
+                }}
+                className="form-control"  aria-label="Username" aria-describedby="basic-addon1"/>
+              </div>
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">Numero de telefono</span>
+                <input type="number"
+                 onChange={(event) => {
+                  setphoneNumber(event.target.value);
+                }}
+                className="form-control"  aria-label="Username" aria-describedby="basic-addon1"/>
+              </div>
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">fecha de arriendo</span>
+                <input type="text"
+                 onChange={(event) => {
+                  setLeaseDate(event.target.value);
+                }}
+                className="form-control"  aria-label="Username" aria-describedby="basic-addon1"/>
+              </div>
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon1">Estado de arriendo</span>
+                <input type="text"
+                 onChange={(event) => {
+                  setState1(event.target.value);
+                }}
+                className="form-control"  aria-label="Username" aria-describedby="basic-addon1"/>
+              </div>
+
+
+              
+
+            </div>
+            <div className="card-footer text-body-secondary">
+              <button className='btn btn-success' onClick={add}> Registrar </button>
 
 
 
-        <div className="inventario1">
-          <div className="datos">
-          <label>ID del Usuario <input 
-            onChange={(event)=>{
-                setIDUsuario(event.target.value);
-            }}
-               type="number"></input></label>
-            
-            <label>Nombre: <input
-            onChange={(event)=>{
-                setNombre(event.target.value);
-            }}
-             type="text"></input></label>
 
-            <label>Apellido: <input 
-            onChange={(event)=>{
-                setApellido(event.target.value);
-            }}
-             type="text"></input></label>
+            </div>
+          </div>
 
-             <label>Tipo de Usuario <input 
-            onChange={(event)=>{
-                setTypeUser(event.target.value);
-            }}
-             type="text"></input></label>
 
-            <label>Contraseña <input 
-            onChange={(event)=>{
-                setContraseña(event.target.value);
-            }}
-              type="text"></input></label>            
-            <label>Identificacion <input type="number"></input></label>
-            <label>CorreoElectronico<input type="text"></input></label>
-            <label>numero de telefono<input
-            onChange={(event)=>{
-                setphoneNumber(event.target.value);
-            }}
-               type="number"></input></label>
-            <label>fecha de arriendo<input type="text"></input></label>
-            <label>Estado de arriendo<input type="number"></input></label>
-            <button onClick={add}> Registrar </button>
 
-          
-          </div>           
+
+
         </div>
 
-{/* 
-        <div className="body-1 col-10 text-center">
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
-          </div>
-        </div> */}
 
       </div>
     </div>
