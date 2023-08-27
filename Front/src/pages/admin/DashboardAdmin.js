21b59570d43feae6df2dbe016aeb300cf2569845
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2023-08-26 20:23:53 -0500

    Arreglo de interfaz

--- Front/src/pages/admin/DashboardAdmin.js
+++ Front/src/pages/admin/DashboardAdmin.js
@@ -1,19 +1,21 @@
 import React from "react";
 import BarraLateral from "../../components/BarraLateral";
-import "../../css/dashboardInicio.css";
 
 const DashboardAdmin = () => {
   return (
-    <div>
-      <div className="d-flex" style={{ minHeight: "78.6vh" }}>
-        <div className="div-barra">
-          <BarraLateral />
-        </div>
-        <div className=" d-flex justify-content-center align-items-center text-center flex-wrap px-3">
-          <div className="pagina-inicio">
-            <div className="div-blanco m-5">
-              <h1>Bienvendio al Modulo de Administración</h1>
-            </div>
+    <div
+      className="d-flex flex-column flex-md-row"
+      style={{ minHeight: "78.6vh" }}
+    >
+      <div className="div-barra">
+        <BarraLateral />
+      </div>
+      <div className="container-fluid d-flex flex-column justify-content-center align-items-center">
+        <div className="error-404">
+          <h1>Edificio K&C</h1>
+          <div className="subtitulo">
+            <h2>Administración</h2>
+            <div className="linea"></div>
           </div>
         </div>
       </div>
--- Front/src/pages/user/DashboardUsuario.js
+++ Front/src/pages/user/DashboardUsuario.js
@@ -1,20 +1,22 @@
 import React from "react";
 import BarraLateral from "../../components/BarraLateral";
 import "../../css/styles.css";
-import "../../css/dashboardInicio.css"
 
 const SistemaUsuario = () => {
   return (
-    <div>
-      <div className="d-flex" style={{ minHeight: "78.6vh" }}>
-        <div className="div-barra">
-          <BarraLateral />
-        </div>
-        <div className=" d-flex justify-content-center align-items-center text-center flex-wrap px-3">
-          <div className="pagina-inicio">
-            <div className="div-blanco m-5">
-              <h1>Bienvendio al Modulo de Arrendatario</h1>
-            </div>
+    <div
+      className="d-flex flex-column flex-md-row"
+      style={{ minHeight: "78.6vh" }}
+    >
+      <div className="div-barra">
+        <BarraLateral />
+      </div>
+      <div className="container-fluid d-flex flex-column justify-content-center align-items-center">
+        <div className="error-404">
+          <h1>Edificio K&C</h1>
+          <div className="subtitulo">
+            <h2>Arrendatario</h2>
+            <div className="linea"></div>
           </div>
         </div>
       </div>
