b279ef3b91de878eda207daa42dec32addfc6a63
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2023-09-02 23:41:53 -0500

    tablas-datos diferentes

---
+++ Front/src/controllers/ZonasControllers.js
@@ -0,0 +1,106 @@
+import Axios from "axios";
+import Swal from "sweetalert2";
+
+//aqui
+export const add = (datos) => {
+  Axios.post("http://localhost:3001/Zonas/create", {
+
+  Id: datos.Id,
+  Nombre: datos.Nombre,
+  Descripcion: datos.Descripcion,
+  Precio: datos.Precio,
+  Acceso: datos.Acceso,
+
+  }).then(() => {
+    Swal.fire({
+      title: "<strong>Registro exitoso</strong>",
+      html:
+        "<i>El arrendatario: <strong>" +
+        datos.Nombre +
+        "</strong> fue registrado</i>",
+      icon: "success",
+      timer: 2000,
+    });
+  });
+};
+
+
+export const update = (datos) => {
+  Axios.put("http://localhost:3001/Zonas/update", {
+      Id: datos.Id,
+  Nombre: datos.Nombre,
+  Descripcion: datos.Descripcion,
+  Precio: datos.Precio,
+  Acceso: datos.Acceso,
+
+
+
+  }).then(() => {
+    Swal.fire({
+      title: "<strong>Actualizacion exitosa</strong>",
+      html:
+        "<i>El arrendatario: <strong>" +
+        datos.Nombre +
+        "</strong> fue actualizado</i>",
+      icon: "success",
+      timer: 2000,
+    });
+  });
+};
+
+export const eliminar = ({ val, getZonas }) => {
+  Swal.fire({
+    title: "ELIMINAR",
+    html:
+      "<i>Desea eliminar la habitacion <strong>" +
+      val.nombre +
+      "</strong> ?  </i>",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Si, eliminar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      /* aqui es donde se elimina */
+      const Id = val.id;
+      Axios.delete("http://localhost:3001/Zonas/delete", {
+        data: { Id: Id },
+      })
+        .then(() => {
+          getZonas();
+          Swal.fire({
+            title: "<strong>Eliminacion exitosa</strong>",
+            html:
+              "<i>El arrendatario: <strong>" +
+              val.nombre +
+              "</strong> fue eliminado</i>",
+            icon: "success",
+            timer: 2000,
+            showConfirmButton: false,
+          });
+        })
+        .catch(function (error) {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "No se pudo eliminar. Intente más tarde",
+            showConfirmButton: false,
+          });
+        });
+    }
+  });
+};
+
+/* traer la infromacion de los arrendatarios */
+export const zonas = async () => {
+  try {
+    const response = await Axios.get(
+      "http://localhost:3001/Zonas/zonas"
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener los datos:", error);
+    return null;
+  }
+};
