0746bade2d0c64dd8f5ef2c75e34cc5ce16b1adb
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2023-09-10 00:10:21 -0500

    validacion zonas

--- Front/src/components/PostZonasModal.js
+++ Front/src/components/PostZonasModal.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { add } from "../controllers/ZonasControllers";
 import "../css/modal.css";
+import Swal from "sweetalert2";
+
 
 const PostZonasModal = ({ getZonas }) => {
   const [Id, setId] = useState("");
@@ -8,7 +10,9 @@ const PostZonasModal = ({ getZonas }) => {
   const [Descripcion, setDescripcion] = useState("");
   const [Precio, setPrecio] = useState("");
   const [Acceso, setAcceso] = useState("");
-
+  const [formularioVisible, setFormularioVisible] = useState(true);
+  
+  
   const limpiarCampos = () => {
     setId("");
     setNombre("");
@@ -16,14 +20,86 @@ const PostZonasModal = ({ getZonas }) => {
     setPrecio("");
     setAcceso("");
   };
-
+  const validateField = (fieldName) => {
+    switch (fieldName) {
+      case "Id":
+        const idPattern = /^\d+$/;
+        if (!idPattern.test(Id) || parseInt(Id, 10) === 0) {
+          setErrorMessages({ ...errorMessages, Id: "El campo Id debe contener solo números y no ser igual a cero." });
+        } else {
+          setErrorMessages({ ...errorMessages, Id: "" });
+        }
+        break;
+      case "Nombre":
+        const nombrePattern = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/u;
+        if (!nombrePattern.test(Nombre) || Nombre.trim().length < 1) {
+          setErrorMessages({ ...errorMessages, Nombre: "El campo Nombre debe contener solo letras y no puede estar vacío." });
+        } else {
+          setErrorMessages({ ...errorMessages, Nombre: "" });
+        }
+        break;
+      case "Precio":
+        const precioPattern = /^\d+$/;
+        if (!precioPattern.test(Precio) || parseInt(Precio, 10) <= 0) {
+          setErrorMessages({ ...errorMessages, Precio: "El campo Precio debe contener solo números y ser mayor que cero." });
+        } else {
+          setErrorMessages({ ...errorMessages, Precio: "" });
+        }
+        break;
+      case "Acceso":
+        const accesoPattern = /^[A-Za-z\s]+$/;
+        if (!accesoPattern.test(Acceso) || Acceso.trim().length < 1) {
+          setErrorMessages({ ...errorMessages, Acceso: "El campo Acceso debe contener solo letras y no puede estar vacío." });
+        } else {
+          setErrorMessages({ ...errorMessages, Acceso: "" });
+        }
+        break;
+      default:
+        break;
+    }
+  };
   const AddPost = () => {
-    add({ Id, Nombre,Descripcion, Precio, Acceso });
-    getZonas();
-    getZonas();
-    limpiarCampos();
+    validateFields();
+    const hasErrors = Object.values(errorMessages).some((message) => message !== "");
+
+    if (!hasErrors) {
+      add({
+        Id,
+        Nombre,
+        Descripcion,
+        Precio,
+        Acceso,
+      });
+
+      getZonas();
+      limpiarCampos();
+      
+      setFormularioVisible(false);
+    } else {
+      mostrarMensajeError(); }
   };
 
+  const validateFields = () => {
+    validateField("Id");
+    validateField("Nombre");
+    validateField("Precio");
+    validateField("Acceso");
+  };
+  const [errorMessages, setErrorMessages] = useState({
+    Id: "",
+    Nombre: "",
+    Precio: "",
+    Acceso: "",
+  });
+  const mostrarMensajeError = () => {
+    Swal.fire({
+      title: "Campos inválidos",
+      text: "Uno o más campos contienen datos inválidos. Por favor, corrige los errores.",
+      icon: "error",
+      showCancelButton: true,
+      confirmButtonText: "Aceptar",
+    });
+  };
   return (
     <div>
       <i
@@ -66,11 +142,18 @@ const PostZonasModal = ({ getZonas }) => {
                   onChange={(event) => {
                     setId(event.target.value);
                   }}
+                  onBlur={() => {
+                    validateField("Id");
+                  }}
                   className="form-control"
                   aria-label="id"
                   aria-describedby="basic-addon1"
                 />
+                {errorMessages.Id && (
+                  <div className="text-danger">{errorMessages.Id}</div>
+                )}
               </div>
+  
               <div className="input-group mb-3">
                 <span className="input-group-text">Nombre</span>
                 <input
@@ -79,11 +162,18 @@ const PostZonasModal = ({ getZonas }) => {
                   onChange={(event) => {
                     setNombre(event.target.value);
                   }}
+                  onBlur={() => {
+                    validateField("Nombre");
+                  }}
                   className="form-control"
                   aria-label="nombre"
                   aria-describedby="basic-addon1"
                 />
+                {errorMessages.Nombre && (
+                  <div className="text-danger">{errorMessages.Nombre}</div>
+                )}
               </div>
+  
               <div className="input-group mb-3">
                 <span className="input-group-text" id="basic-addon1">
                   Descripción
@@ -99,21 +189,29 @@ const PostZonasModal = ({ getZonas }) => {
                   aria-describedby="basic-addon1"
                 />
               </div>
+  
               <div className="input-group mb-3">
                 <span className="input-group-text" id="basic-addon1">
                   Precio
                 </span>
                 <input
-                  type="price"
+                  type="text"
                   value={Precio}
                   onChange={(event) => {
                     setPrecio(event.target.value);
                   }}
+                  onBlur={() => {
+                    validateField("Precio");
+                  }}
                   className="form-control"
-                  aria-label="precio"
+                  aria-label="Precio"
                   aria-describedby="basic-addon1"
                 />
+                {errorMessages.Precio && (
+                  <div className="text-danger">{errorMessages.Precio}</div>
+                )}
               </div>
+  
               <div className="input-group mb-3">
                 <span className="input-group-text" id="basic-addon1">
                   Tipo de acceso
@@ -124,10 +222,16 @@ const PostZonasModal = ({ getZonas }) => {
                   onChange={(event) => {
                     setAcceso(event.target.value);
                   }}
+                  onBlur={() => {
+                    validateField("Acceso");
+                  }}
                   className="form-control"
                   aria-label="acceso"
                   aria-describedby="basic-addon1"
                 />
+                {errorMessages.Acceso && (
+                  <div className="text-danger">{errorMessages.Acceso}</div>
+                )}
               </div>
             </div>
             <div className="modal-footer">
@@ -142,7 +246,7 @@ const PostZonasModal = ({ getZonas }) => {
               <button
                 type="button"
                 className="btn btn-primary"
-                data-bs-dismiss="modal"
+                //data-bs-dismiss="modal"
                 onClick={AddPost}
               >
                 Agregar
