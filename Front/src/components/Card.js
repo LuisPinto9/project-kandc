5bb0cced5c43a7209f73eeb7c4671cb71f1dda9a
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2023-09-18 12:29:40 -0500

    Arreglo página de inicio

--- Front/src/components/Card.js
+++ Front/src/components/Card.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import imagenp1 from "../images/imagenp1.png";
-// import imagenp2 from "../images/imagenp2.png";
-// import imagenp3 from "../images/imagenp3.png";
 import { getZonasInicio } from "../controllers/ZonasControllers.js";
 
 const Card = () => {
@@ -20,50 +18,98 @@ const Card = () => {
     getZonas();
   }, []);
 
+  const groupSize = 3;
+
+  const groupZones = () => {
+    const grouped = [];
+    for (let i = 0; i < zones.length; i += groupSize) {
+      grouped.push(zones.slice(i, i + groupSize));
+    }
+    return grouped;
+  };
+
+  const groupedZones = groupZones();
+
+  function getHabitacionesText(id) {
+    if (id === 102) {
+      return "2";
+    } else {
+      return "3";
+    }
+  }
+
   return (
     <div>
       <div className="d-flex justify-content-center flex-wrap div-cards">
-        {zones &&
-          zones.map((zone) => {
-            // Obtener el último dígito del ID
-            const lastDigit = zone.id % 10;
-            // Definir el texto de Habitaciones en función del último dígito
-            let habitacionesText = "";
-            if (zone.id === 102) {
-              habitacionesText = "Habitaciones: 2";
-            } else if (lastDigit === 2) {
-              habitacionesText = "Habitaciones: 3";
-            } else if (lastDigit === 1) {
-              habitacionesText = "Habitaciones: 4";
-            }
-            return (
-              <div className="div-imagenp pt-3 px-3 m-3" key={zone.id}>
+        <div className="d-flex col-12">
+          <div
+            id="carouselExampleInterval"
+            className="carousel slide"
+            data-bs-ride="carousel"
+            style={{ width: "93vw" }}
+          >
+            <div className="carousel-inner" data-bs-interval="5000">
+              {groupedZones.map((group, index) => (
                 <div
-                  className="card"
-                  style={{
-                    width: "18rem",
-                    borderColor: "white",
-                    border: "0rem",
-                    borderTopLeftRadius: "10rem",
-                    borderTopRightRadius: "10rem",
-                  }}
+                  className={`carousel-item ${index === 0 ? "active" : ""}`}
+                  key={index}
                 >
-                  <img src={imagenp1} className="card-img-top" alt="..." />
-                  <div className="card-body pt-0">
-                    <div className="card-text-title">{zone.nombre}</div>
-                    <p className="card-text">
-                      Precio: ${zone.precio.toLocaleString()}
-                      <br />
-                      {habitacionesText}
-                      <br />
-                      Servicios: Agua, luz, gas
-                      <br />
-                    </p>
+                  <div className="d-flex justify-content-center flex-wrap">
+                    {group.map((zone) => (
+                      <div className="div-imagenp pt-3 px-3 m-3" key={zone.id}>
+                        <div
+                          className="card"
+                          style={{
+                            width: "18rem",
+                            borderColor: "white",
+                            border: "0rem",
+                            borderTopLeftRadius: "10rem",
+                            borderTopRightRadius: "10rem",
+                          }}
+                        >
+                          <img
+                            src={imagenp1}
+                            className="card-img-top"
+                            alt="..."
+                          />
+                          <div className="card-body pt-0">
+                            <div className="card-text-title">{zone.nombre}</div>
+                            <p className="card-text">
+                              Precio: ${zone.precio.toLocaleString()}
+                              <br />
+                              Habitaciones: {getHabitacionesText(zone.id)}
+                              <br />
+                              Servicios: Agua, luz, gas
+                              <br />
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              ))}
+            </div>
+            <i
+              className="bi bi-chevron-compact-left carousel-control-prev"
+              data-bs-target="#carouselExampleInterval"
+              data-bs-slide="prev"
+              type="button"
+              style={{ fontSize: "3rem", color: "gray" }}
+            >
+              <span className="visually-hidden">Previous</span>
+            </i>
+            <i
+              className="bi bi-chevron-compact-right carousel-control-next"
+              data-bs-target="#carouselExampleInterval"
+              data-bs-slide="next"
+              type="button"
+              style={{ fontSize: "3rem", color: "gray" }}
+            >
+              <span className="visually-hidden">Next</span>
+            </i>
+          </div>
+        </div>
       </div>
     </div>
   );
