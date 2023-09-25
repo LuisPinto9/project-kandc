070d0f9113da0c7374c1d5b200a221d979ca9357
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2023-09-25 16:29:09 -0500

    tutorial

--- Front/src/pages/Home.js
+++ Front/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../components/Card.js";
+import manual from "../video/manualusuario.mp4";
 
 function Home() {
   return (
@@ -20,6 +21,20 @@ function Home() {
         <div className="p-2 mt-4">
           <Card />
         </div>
+        <div className="p-2 mt-4">
+          <div>
+            <h2 style={{ textAlign: "center", fontFamily:"Roboto Mono" }}>
+              Tutorial de explicación de la página
+            </h2>
+          </div>
+          <video style={{ width: "1000px", height: "570px" }} controls>
+            <source src={manual} type="video/mp4" />
+            Tu navegador no admite la reproducción de video.
+          </video>
+        </div>
+        
+
+
       </div>
     </div>
   );
